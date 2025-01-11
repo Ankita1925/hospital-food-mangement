e29accff27362295f155425256f59ebbf092c965
Ankita1925 <ankitasawant22123071@gmail.com>
2025-01-11 19:38:18 +0530

    Update axiosConfig.js

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with a base URL
 const apiClient = axios.create({
-    baseURL: "https://hospital-food-mangement-backend.onrender.com/", // Replace with your backend URL
+    baseURL: "https://hospital-food-mangement-backend.onrender.com", // Replace with your backend URL
     headers: {
         "Content-Type": "application/json",
     },
